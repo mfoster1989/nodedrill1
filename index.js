a95b674dd5b3c26586449b64e20fae38c52d1806
mfoster1989 <malcolmfoster@gmail.com>
2017-11-15 18:21:42 -0700

    backup

--- index.js
+++ index.js
@@ -6,42 +6,52 @@ app.use(cors());
 const cohorts =
 [{
     id: 1,
-    cohortName: 17-01-WD-DP,
-    cohortCode: g100,
+    cohortName: "17-01-WD-DP",
+    cohortCode: "g100",
     numberOfStudents: 28
   },{
     id: 2,
-    cohortName: 17-01-DS-GT,
-    cohortCode: g105,
+    cohortName: "17-01-DS-GT",
+    cohortCode: "g105",
     numberOfStudents: 24
   },{
     id: 3,
-    cohortName: 17-02-WD-PX,
-    cohortCode: g109,
+    cohortName: "17-02-WD-PX",
+    cohortCode: "g109",
     numberOfStudents: 30
   },{
     id: 4,
-    cohortName: 17-03-WD-BD,
-    cohortCode: g110,
+    cohortName: "17-03-WD-BD",
+    cohortCode: "g110",
     numberOfStudents: 29
   }]
 
+app.use(cors());
+
 function idQuery (id, array) {
-  for (var i = 0; i < array.id.length; i++) {
-    if (array.id[i] === id) {
-      return array[i]
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].id == id) {
+      return array[i];
     }
   }
+  return null
 }
 
-
-app.get('/', function (req, res) {
-  res.json(msg:'This is CORS-enabled for all origins!')
+app.get("/", function (req, res) {
+  res.json(cohorts)
 })
 
+app.get("/:id", function (req, res) {
+  var record = idQuery(cohorts, req.params.id);
+  if (!record) {
+    res.status = 404;
+    res.json({
+      error: {
+        message: "No record found!"
+      }
+    });
+  }
+  res.json({array: record});
+});
 
-app.get('/:id' function (req, res) {
-  let path = req.params.id
-  idQuery (path, cohorts);
-  res.json(msg:'This is CORS-enabled for all origins!')
-})
+app.listen(3000);
